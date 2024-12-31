b49af23935829794362ca3cb7f7bcfa3c310d36d
axing9409 <19938055407@163.com>
2024-12-31 17:59:54 +0800

    F

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1', // 或者使用 true
+    port: 10081, // 你可以自定义端口
+  },
 })
